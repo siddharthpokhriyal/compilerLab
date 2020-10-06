3e848a5ffafbbaf8a707a4fc00b946f445fd9486
siddharthpokhriyal <siddharth7pokhriyal@gmail.com>
2020-10-06 20:44:38 +0530

    24/8/2020

---
+++ Strings.py
@@ -0,0 +1,28 @@
+PROGRAM TO CHECK WHETHER A STRING IS ACCEPTED OR NOT FOR A GRAMMAR – ab*c(a+b)
+
+
+def py():
+    l = []
+    str = "ab*c(a+b)"
+    pstr = ["aca", "acb"]
+    print ("Siddharth Pokhriyal")
+    print ("1816110205")
+    n = input("Enter the string: ")
+    if n[len(n) - 2] != "c":
+        print("String not accepted")
+    elif n in pstr:
+        print("String Accepted")
+    elif n[1] == "b":
+        for i in range(len(n)):
+            if n[i] == "b" and i != len(n) - 1:
+                pass
+            else:
+                l.append(n[i])
+        p = "".join(l)
+        if p in pstr:
+            print("String accepted")
+        else:
+            print("String not accepted")
+    else:
+        print("String not accepted")
+py()
